c123911dda9d375d4aa3a441f7121e2e25edee6f
fiy <15910740557@163.com>
2018-08-25 16:12:22 +0800

    zhongjianxindegongneng

---
+++ app/src/main/java/com/example/demo/fragment/OneFragment.java
@@ -0,0 +1,6 @@
+package com.example.demo.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class OneFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/demo/fragment/TwoFragment.java
@@ -0,0 +1,19 @@
+package com.example.demo.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.demo.R;
+
+public class OneFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
